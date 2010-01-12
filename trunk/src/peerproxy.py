dcecfe7487a5e947a4a6b292e1e5f53e05375da1
Vivek Sekhar <vsekhar@xmain.com>
2010-01-12 06:09:10 +0000

    Delete trunk (moved to tags/2009-07-21_pre-swig_xml-rpc) to be replaced with non-threaded asyncore/asynchat from assembla/pymesh space
    git-svn-id: file:///home/vsekhar/vgp/vmesh@267 cb3134f3-094e-0410-9174-ff1f5a1024ca

--- trunk/src/peerproxy.py
+++
@@ -1,7 +0,0 @@
-import xmlrpc.client
-
-class PeerProxy:
-    def __init__(self, addr_port):
-        self.host, self.port = addr_port
-        self.uri = "http://" + self.host + ":" + str(self.port)
-        self.do = xmlrpc.client.ServerProxy(self.uri)
