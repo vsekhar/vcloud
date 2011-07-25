49cc1a7b423801486d86782dfbcb35a00845a0cd
Vivek Sekhar <vivek@xmain.com>
2011-07-25 16:14:11 -0400

    Credentials template file

---
+++ CREDENTIALS_template.py
@@ -0,0 +1,17 @@
+# 1. fill out required fields, enclosing strings in quotes
+# 2. rename file 'CREDENTIALS.py' and place in same directory as cloudlaunch.py
+
+# AWS credentials
+access_key = ''
+secret_key = ''
+
+# S3 bucket and package file
+bucket = ''
+package = ''
+
+# Name of script inside package to run on startup
+script = ''
+
+# Username to run script as
+user = ''
+
